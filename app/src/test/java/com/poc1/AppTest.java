da374b7f70ea8ba32ed0e1ef28158d59ba4d3080
Ironman680-0202 <sandeep.chandra52361234@gmail.com>
2026-04-26 20:09:50 +0530

    final commit

--- app/src/test/java/com/poc1/AppTest.java
+++ app/src/test/java/com/poc1/AppTest.java
@@ -1,26 +1,12 @@
 package com.poc1;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-class AppTest {
-
-    @Autowired
-    private MockMvc mockMvc;
+public class AppTest {
 
     @Test
-    void homeEndpointShouldReturnExpectedMessage() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("POC-1 DevSecOps Application is running"));
+    public void testAdd() {
+        assertEquals(20, App.add(10, 10));
     }
 }
