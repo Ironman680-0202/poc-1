4bab4129c9fbff76f574e1878450f704e6c09e9a
Ironman680-0202 <sandeep.chandra52361234@gmail.com>
2026-04-26 20:01:49 +0530

    update java file

--- app/src/main/java/com/poc1/App.java
+++ app/src/main/java/com/poc1/App.java
@@ -1,12 +1,20 @@
 package com.poc1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("POC-1 DevSecOps Application is running");
+        SpringApplication.run(App.class, args);
     }
 
-    public static int add(int a, int b) {
-        return a + b;
+    @GetMapping("/")
+    public String home() {
+        return "POC-1 DevSecOps Application is running";
     }
 }
